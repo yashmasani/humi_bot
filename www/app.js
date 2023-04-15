0422ffcdf0521526affeb9edd8f1ec8f6525b835
Yash Masani <yash.masani@gmail.com>
2023-04-15 19:24:19 -0400

    added scheduler for web scraping and scheduled message posting

--- www/app.js
+++ www/app.js
@@ -1,9 +1,12 @@
 const { App } = require("@slack/bolt");
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 const { find_today } = require("wasm-build");
 require("dotenv").config();
-const api = require('./api');
 const navigate = require('./navigate');
-const fetch = require("node-fetch");
+const { schedule, validateWebScrapingTime }= require('./scheduler');
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -12,7 +15,8 @@ const app = new App({
   appToken: process.env.APP_TOKEN,
 });
 
-
+// INITIALIZE SCHEDULE TIME;
+let SCHEDULED_TIME;
 
 //events
 app.event('app_home_append',async ({event})=>{
@@ -41,20 +45,30 @@ app.command('/time_off/help', async ({ command, ack, respond})=>{
   }
 });
 
+async function runTimeOffEvents(startTime) {
+  let timeInterval = null;
+   
+  const date = dayjs().tz('America/Toronto');
+  if (validateWebScrapingTime(date) && !SCHEDULED_TIME) {
+    try {
+      console.time('nav');
+      const html = await navigate('https://hr.humi.ca/login');
+      console.timeEnd('nav');
+      console.log(find_today(html));
+    } catch(e) {
+      console.error(e);
+    }
+  }
+  
+}
 
 (async () => {
   const port = 3000
   // Start your app
-  await app.start(process.env.PORT || port);
-  console.time('nav');
-  const html = await navigate('https://hr.humi.ca/login');
-  console.timeEnd('nav');
   try {
-    console.log(find_today(html));
+    await app.start(process.env.PORT || port);
+    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
   } catch(e) {
     console.error(e);
-    console.log('Cannot find today');
-  } finally {
-    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
   }
 })();
--- www/scheduler.js
+++ www/scheduler.js
@@ -5,7 +5,27 @@ const timezone = require('dayjs/plugin/timezone');
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-function schedule() {
-  const date = dayjs().tz('America/Toronto');
-  
+// TIME IS 24HR
+// dayjs HOUR BETWEEN 0-23
+function convertToDayJS (x) {
+  return x - 1;
 }
+
+function schedule(date) {
+  const SCHEDULE = 10; //10AM in 23hr DAYJS
+  const dateAtSchedule = date.hour(convertToDayJS(SCHEDULE));
+  return dateAtSchedule.unix();
+}
+
+// validate Time is between 6-9am for web scraping
+function validateWebScrapingTime(date) {
+  const TIME_START = 6;
+  const TIME_STOP = 9;
+  
+  // weekday and expected time
+  const isWeekDay = date.day() >= 1 && date.day() <= 5;
+  const betweenExpectedTime = date.hour() >= convertToDayJS(TIME_START) && date.hour() <= convertToDayJS(TIME_STOP);
+  return isWeekDay && betweenExpectedTime;
+}
+
+module.exports = { schedule, validateWebScrapingTime };
