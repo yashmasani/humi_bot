3524858fa3482bc41f621d044e35055a42130bd9
Yash Masani <yash.masani@gmail.com>
2023-04-16 15:39:07 -0400

    updated dayjs instance to not use local time zone

--- www/test/scheduler.spec.js
+++ www/test/scheduler.spec.js
@@ -1,12 +1,16 @@
 const assert = require('node:assert').strict;
 const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 const scheduler = require('../scheduler');
 
 describe('Scheduler', function() {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
   describe('schedule time', function() {
     it('Accurate Scheduled Time', function() {
       const date = Date.parse('2023-04-15T06:00:43-04:00');
-      const actual = scheduler.schedule(dayjs(date));
+      const actual = scheduler.schedule(dayjs(date).tz('America/Toronto'));
       // expect in seconds EPOCH
       const expect = (Date.parse('2023-04-15T10:00:43-04:00'))/1000;
       assert.equal(actual, expect, `schedule time does not match with 10am: \n
@@ -17,14 +21,14 @@ describe('Scheduler', function() {
   describe('calculate interval time', function() {
     it('Testing Interval Functionality before target time', function() {
       const date = Date.parse('2023-04-15T06:00:00-04:00');
-      const actual = scheduler.calculateInterval(dayjs(date), 10);
+      const actual = scheduler.calculateInterval(dayjs(date).tz('America/Toronto'), 10);
       // expect in seconds EPOCH
       const expect = (Date.parse('2023-04-15T10:00:00-04:00') - date);
       assert.equal(actual, expect, 'calculate interval time does not match');
     });
     it('Testing Interval Functionality after target time', function() {
       const date = Date.parse('2023-04-15T15:00:00-04:00');
-      const actual = scheduler.calculateInterval(dayjs(date), 10);
+      const actual = scheduler.calculateInterval(dayjs(date).tz('America/Toronto'), 10);
       // expect in seconds EPOCH
       const expect = (Date.parse('2023-04-16T10:00:00-04:00') - date);
       assert.equal(actual, expect, 'calculate interval time does not match');
