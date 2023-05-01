f107c5a406dc8aee55d6e82899d57347ad0885f4
Yash Masani <yash.masani@gmail.com>
2023-04-30 22:46:45 -0400

    added prod functionality

--- www/app.js
+++ www/app.js
@@ -12,8 +12,8 @@ dayjs.extend(timezone);
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.APP_TOKEN,
+  /*socketMode: true,
+  appToken: process.env.APP_TOKEN,*/
 });
 
 //events
