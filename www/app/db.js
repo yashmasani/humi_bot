1ea86c96d9e3370b2c9cadac69ee7f0e9ce8e6b6
Yash Masani <yash.masani@gmail.com>
2023-05-12 01:07:50 -0400

    added db

--- www/app/db.js
+++ www/app/db.js
@@ -1,21 +1,60 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
 
 function getTable() {
+  try {
+    const db = new sqlite3.Database(':memory:');
+    db.run(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY NOT NULL,
+        installation TEXT NOT NULL,
+        team_id TEXT NOT NULL,
+        channel TEXT
+      )`
+    );
+  } catch(e) {
+    console.error('Unable to start db');
+    console.error(e);
+  }
   // create the table
   return db;
 }
 
-function saveInstall() {
-  //pass
+function saveInstall(db, installationItem) {
+  if (installationItem && installationItem.team && installation.token && installation.id) {
+    db.run(`
+      INSERT INTO users(installation)
+      VALUES (?)
+    `, JSON.stringify(installationItem),
+    function(err) {
+      console.error('Unable to authenticate installation');
+      console.error(err);
+    });
+  }
 }
 
-function getInstall() {
-  //pass
+function getInstall(db, teamId) {
+  if (teamId) {
+    db.get('SELECT installation FROM users WHERE team_id=?', teamId, function(err, row) {
+      if (!err || !row) {
+        console.error('Unable to get installation item');
+        console.error(err);
+      } else if (row && !row.installation) {
+        console.error('Non valid Installation Item');
+      } else {
+        return row.installation;
+      }
+    })
+  }
 }
 
-function delInstall() {
-  //pass
+function delInstall(db, teamId) {
+  if (teamId) {
+    db.run('DELETE FROM users WHERE team_id=?', teamId, function(err) {
+      if(err) {
+        console.error(e);
+      }
+    });
+  }
 }
 
 
